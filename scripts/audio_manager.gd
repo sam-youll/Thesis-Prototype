f87bca39ac1ef07634b8d85b7f096fd3b3d80d1e
sydastry <165812966+sydastry@users.noreply.github.com>
2024-09-26 19:58:02 -0400

    Taking out vestigial audio manager code
    took out references to the old fmod project

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -2,9 +2,7 @@ extends Node3D
 class_name AudioManager
 #declaringevent variables
 @onready var ambience : FmodEvent = null
-@onready var music  : FmodEvent = null
-@onready var character_sound: FmodEventEmitter3D = $EventEmitterCharacter
-
+@onready var character_sound: FmodEvent = null
 
 @onready var player: PlayerCharacter
 
@@ -13,16 +11,14 @@ func _ready() -> void:
 	player = get_parent()
 	#initializing events
 	ambience = FmodServer.create_event_instance("event:/Ambience")
-	music = FmodServer.create_event_instance("event:/Music")
 	#character_sound = FmodServer.create_event_instance("event:/Character Sound")
 	#playing events
 	ambience.start()
-	music.start()
 	#character_sound.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	character_sound["event_parameter/SpeedVolume/value"] = player.volume
+	character_sound["event_parameter/PlayerSpeed/value"] = player.volume
 
 func _on_event_emitter_character_started() -> void:
 	print("character sound started")
