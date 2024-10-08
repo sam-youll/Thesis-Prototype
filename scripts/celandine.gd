171a2a35339a8ff5e4872fccc698f8160dcc6da4
sydastry <165812966+sydastry@users.noreply.github.com>
2024-10-08 01:36:58 -0400

    new chime
    new sound for the flowers

--- scripts/celandine.gd
+++ scripts/celandine.gd
@@ -5,9 +5,9 @@ class_name Celandine
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	print("area entered")
-	FmodServer.play_one_shot("event:/mus_lead")
+	FmodServer.play_one_shot("event:/mus_chime")
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	print("body entered")
-	FmodServer.play_one_shot("event:/mus_lead")
+	FmodServer.play_one_shot("event:/mus_chime")
